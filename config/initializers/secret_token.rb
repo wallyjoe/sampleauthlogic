ecdbefa9037955ecbc3c8efb0d15b93f63e6d2a6
wally <wallyjoe8413@gmail.com>
2010-11-05 00:37:37 -0400

    testing authlogic on rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleauthlogic::Application.config.secret_token = '06999e19dbeac131ab4a0af3c06123ccb44963c9e447a0491b3f44825d29dd80169e0ac0cf6ecb5dbd09291de59902a155427a3ab0cc220b0111975080fcca15'
